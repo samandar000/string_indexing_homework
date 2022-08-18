7812e51e9ce23e4bdd461c44f7687a3c9377f3cd
Samandar000 <samandarnfh.2020@gmail.com>
2022-08-18 16:12:22 +0500

    099

--- Index09.py
+++ Index09.py
@@ -8,24 +8,34 @@ def main(s):
     """
     if s=="0":
         n=0
+    
     elif s=="1":
         n=1
+    
     elif s=="2":
         n=2
+    
     elif s=="3":
         n=3
+    
     elif s=="4":
         n=4
+    
     elif s=="4":
         n=4
+    
     elif s=="5":
         n=5
+    
     elif s=="6":
         n=6
+    
     elif s=="7":
         n=7
+    
     elif s=="8":
         n=8
+    
     elif s=="9":
         n=9
     else:
